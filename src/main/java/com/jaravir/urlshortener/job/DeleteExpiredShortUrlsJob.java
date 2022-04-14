c82b6b32c963ec519a163bb0b26f315421ed4e1a
Samir Jabrayilov <jaravir@gmail.com>
2022-04-14 20:50:40 +0200

    Add comments to DeleteExpiredShortUrlsJob

--- src/main/java/com/jaravir/urlshortener/job/DeleteExpiredShortUrlsJob.java
+++ src/main/java/com/jaravir/urlshortener/job/DeleteExpiredShortUrlsJob.java
@@ -2,6 +2,10 @@ package com.jaravir.urlshortener.job;
 
 import com.jaravir.urlshortener.store.ShortUrlStore;
 
+/**
+ * Periodic job to delete expired short urls. There are no tests for it because it just delegates
+ * to the {@link ShortUrlStore#deleteExpiredShortUrls()} which is tested under its own unit tests.
+ */
 public class DeleteExpiredShortUrlsJob implements Runnable {
   private ShortUrlStore store;
 
